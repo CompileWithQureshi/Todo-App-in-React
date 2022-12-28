de495120e19307b67bfb0da128eac1015f681f74
contactme.qureshi@gmail.com <contactme.qureshi@gmail.com>
2022-12-28 07:40:44 -0800

    first commit

---
+++ src/TodoApp.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './App.css'
+
+function TodoList() {
+    const [todos, setTodos] = useState([]);
+    const [input, setInput] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setTodos([...todos, input]);
+        console.log([...todos, input]);
+        setInput('');
+    }
+
+    const handleDelete = (index) => {
+        setTodos(todos.filter((todo, i) => i !== index));
+    }
+
+    return (
+        <div className='container'>
+            <div className='main'>
+                <h1>Todo-List</h1>
+                <form onSubmit={handleSubmit}>
+                    <input value={input} onChange={(e) => setInput(e.target.value)} className='input' />
+                    <button type="submit" className='input-btn'>Add Todo</button>
+                </form>
+                <ul style={{ listStyle: 'number' }}>
+                    {todos.map((todo, index) => (
+                        <li key={index} >
+                            {todo}
+                            <button onClick={() => handleDelete(index)}>Delete</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default TodoList;
